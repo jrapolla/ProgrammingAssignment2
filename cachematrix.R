febf29fc57f8078afbea5ce364e9a327b6a1e787
Joseph Rapolla <josephrapolla@gmail.com>
2015-03-21 14:15:22 -0700

    Assignment 2 Changes
    Added code and comments as per assignment 2 instructions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions demonstrate the use of cache by assigning values to an environment different than the current environment
+## and performing a function to return a value from cache without needing to compute the function
 
-## Write a short comment describing this function
+## This function creates a matrix that stores its inverse to cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse, 
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function looks to compute the inverse of the matrix returned by the
+## makeCacheMatrix function and if no changes, will return the value from cache 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
